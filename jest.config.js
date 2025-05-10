f10163cc13ba541ed145d66d975cbe18920deb83
SNComputerGuru <shillingndivho4@gmail.com>
2025-05-10 20:58:45 +0200

    add tests for all models

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,20 @@
+// module.exports = {
+//   collectCoverage: true,
+//   coverageDirectory: "coverage",
+//   coverageReporters: ["text", "lcov"],
+// };
+
+// Option 1: In jest.config.js
 module.exports = {
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageReporters: ["text", "lcov"],
+  // Your other Jest configuration...
+  setupFiles: ['./jest-setup.js']
 };
+
+// Option 2: In package.json
+/*
+{
+  "jest": {
+    "setupFiles": ["./jest-setup.js"]
+  }
+}
+*/
