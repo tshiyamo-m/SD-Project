72f64c82a562f66568d615a988249f597b3e0008
Ndivho Shilling <140017972+SNComputerGuru@users.noreply.github.com>
2025-05-10 21:09:45 +0200

    Update jest.config.js to what it was before the previous commit

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,5 @@
-// module.exports = {
-//   collectCoverage: true,
-//   coverageDirectory: "coverage",
-//   coverageReporters: ["text", "lcov"],
-// };
-
-// Option 1: In jest.config.js
 module.exports = {
-  // Your other Jest configuration...
-  setupFiles: ['./jest-setup.js']
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"],
 };
-
-// Option 2: In package.json
-/*
-{
-  "jest": {
-    "setupFiles": ["./jest-setup.js"]
-  }
-}
-*/
